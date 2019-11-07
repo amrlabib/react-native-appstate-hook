2a41b1cf503a221a1389c8b544cd5a5dc517b962
bobinrinder <robin@m56studios.com>
2019-11-06 16:46:10 -0800

    moved handleAppStateChange function into useEffect

--- dist/index.js
+++ dist/index.js
@@ -5,33 +5,30 @@ export default function useAppState(settings) {
   const { onChange, onForeground, onBackground } = settings || {};
   const [appState, setAppState] = useState(AppState.currentState);
 
-  function handleAppStateChange(nextAppState) {
-    if (nextAppState === 'active') {
-      isValidFunction(onForeground) && onForeground();
-    } else if (
-      appState === 'active' &&
-      nextAppState.match(/inactive|background/)
-    ) {
-      // to ensure that onBackground() is not called twice we
-      // check if the previous app state was 'active' as iOS
-      // has two not-active-states (inactive and background)
-      isValidFunction(onBackground) && onBackground();
-    }
-    setAppState(nextAppState);
-    isValidFunction(onChange) && onChange(nextAppState);
-  }
-
   // didMount effect
   useEffect(() => {
+    function handleAppStateChange(nextAppState) {
+      if (nextAppState === 'active') {
+        isValidFunction(onForeground) && onForeground();
+      } else if (
+        appState === 'active' &&
+        nextAppState.match(/inactive|background/)
+      ) {
+        // to ensure that onBackground() is not called twice we
+        // check if the previous app state was 'active'
+        // as iOS has two not-active-states (inactive and background)
+        isValidFunction(onBackground) && onBackground();
+      }
+      setAppState(nextAppState);
+      isValidFunction(onChange) && onChange(nextAppState);
+    }
     AppState.addEventListener('change', handleAppStateChange);
     // didUnmount effect
     return () => AppState.removeEventListener('change', handleAppStateChange);
-  },[]);
-
+  }, [onChange, onForeground, onBackground, appState]);
   // settings validation
   function isValidFunction(func) {
     return func && typeof func === 'function';
   }
-
   return { appState };
 }
